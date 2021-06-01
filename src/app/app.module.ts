416a2a43a221bda210aa477c305f47c36e39dfd7
mbonifasi <mbonifasi-2019139@kinal.edu.gt>
2021-06-01 12:12:08 -0600

    Inicio Gestor de Torneos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
